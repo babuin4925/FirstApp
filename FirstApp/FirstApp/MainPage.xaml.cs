bbab72f224b56dc644e187710218278c72489695
Imache <babuin4925@gmail.com>
2021-10-18 20:12:33 +0300

    ButtonClick

--- FirstApp/FirstApp/MainPage.xaml.cs
+++ FirstApp/FirstApp/MainPage.xaml.cs
@@ -10,9 +10,22 @@ namespace FirstApp
 {
     public partial class MainPage : ContentPage
     {
+        private StackLayout layout;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public int Counter{ get; private set; }
+
+        protected override void OnAppearing()
+        {
+            Counter = 0; //default counter value
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Counter++;
+            CounterLabel.Text = $"You clicked {Counter} times";
         }
     }
 }
--- FirstApp/FirstApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ FirstApp/FirstApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace FirstApp {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label CounterLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            CounterLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "CounterLabel");
         }
     }
 }
